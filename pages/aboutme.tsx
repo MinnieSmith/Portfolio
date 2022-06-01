152a39e68f59ee11e25ebeb8e02e224762cd53f2
Minh Smith <minnie@192-168-1-111.tpgi.com.au>
2022-06-01 20:44:40 +0800

    Looking good!

---
+++ pages/aboutme.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from 'next'
+import Footer from '../components/footer'
+import React from 'react'
+import ProjectLayout from '../components/projectslayout'
+import BlogBlock from '../components/blogblock';
+
+
+const AboutMe: NextPage = () => {
+
+  return (
+    <div className="bg-red-50 flex w-screen h-screen">
+      <title>About Me</title>
+      <div className='z-40 top-6 left-4 text-xl md:text-3xl opacity-90 absolute md:top-8 md:left-10 sm:top-8 sm:left-10 sm:text-2xl font-semibold text-white'> about me</div>
+      <div className='rounded-lg flex-none w-32 sm:w-44 blur-sm md:w-52 relative bg-fixed bg-cover bg-[url("/gantheume.jpeg")]'>
+      </div>
+      <ProjectLayout>
+        <BlogBlock />
+        <div className='shadow-xl lg:mt-16 lg:mb-16 lg:ml-40 lg:mr-40 lg:p-5 sm:m-6 sm:p-3 mb:m-4 mb:p-2 opacity-90 bg-sky-50'>
+          <img className="grow mb:mx-auto rounded-lg shadow-xl" src="/deskdog2.jpeg" />
+          <div className='p-2 text-slate-700'>
+            <figcaption>
+              This photos was taken literally as I'm writing this page!
+            </figcaption>
+          </div>
+        </div>
+        <Footer />
+      </ProjectLayout>
+    </div>
+  );
+};
+
+export default AboutMe
